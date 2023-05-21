e8174b2268ae60c0a4de774cfe76b8941bf79dbf
Парфенцов Илья Александрович <regularpencil@yandex.ru>
2023-05-21 16:00:26 +0700

    edit path for images directory

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
-
+import * as path from 'path';
 @Controller()
 export class ImageController {
     @Get("/uploads/images/:filename")
     async getFile(@Param("filename") filename: string, @Res() res: any) {
-        res.sendFile(filename, { root: 'uploads/images' });
+        const rootDirectory = path.join(process.cwd(), 'uploads/images');
+        console.log(rootDirectory);
+        res.sendFile(filename, { root: rootDirectory });
     }
 }
