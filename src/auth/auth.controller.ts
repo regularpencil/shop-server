d9217bb6df66e05c2dc086fa46c5e4a91e7269eb
Парфенцов Илья Александрович <regularpencil@yandex.ru>
2023-06-09 00:39:27 +0700

    change redirect logic 2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -52,7 +52,7 @@ export class AuthController {
         @Param('activationLink') activationLink: string,
         @Res({ passthrough: true }) response: Response
     ) {
-        response.redirect("https://shop-server-eight.vercel.app/api/auth/activate");
+        response.redirect("https://shop-server-eight.vercel.app/api/auth/activated");
         return await this.authService.activate(activationLink);
     }
 
