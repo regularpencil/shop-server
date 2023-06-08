5f2ac58d22e7b14a8991b6a2e5f043902caa497a
Парфенцов Илья Александрович <regularpencil@yandex.ru>
2023-06-08 18:19:41 +0700

    edit cors rules

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   app.use(cookieParser());
   app.enableCors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
     credentials: true,
-    origin: true,
-    methods: ["GET", "POST", "PUT", "DELETE"]
   });
   await app.listen(4000, () => { console.log(`server port - 4000`) });
 }
