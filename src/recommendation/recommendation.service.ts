cd174d0f20a79071c2eadf5ca9c9ad36cb7789e9
Парфенцов Илья Александрович <regularpencil@yandex.ru>
2023-06-20 23:56:05 +0700

    change recomendation cycle

--- src/recommendation/recommendation.service.ts
+++ src/recommendation/recommendation.service.ts
@@ -25,7 +25,7 @@ export class RecommendationService {
       const data = JSON.stringify({goods, users});
       console.log(__filename);
       console.log(workerThreadFilePath);
-      new Worker('./src/workers/worker.js', {workerData: data});
+      new Worker('../workers/worker.js', {workerData: data});
     }
 
     async getRecomendation(userEmail: string) {
